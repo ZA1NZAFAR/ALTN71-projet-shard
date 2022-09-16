4bca452b682d09f7940302465b83e8a2578242ed
Ousseynou <osakho62@gmail.com>
2022-09-16 16:19:41 +0200

    Initialisation_Projet

---
+++ Shard.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Shard.Api;
+using Shard.Shared.Web.IntegrationTests;
+using Xunit.Abstractions;
+
+namespace Shard.IntegrationTests;
+
+public class IntegrationTests : BaseIntegrationTests<Program>
+{
+    public IntegrationTests(
+        WebApplicationFactory<Program> factory,
+        ITestOutputHelper testOutputHelper)
+        : base(factory, testOutputHelper)
+    {
+    }
+}
