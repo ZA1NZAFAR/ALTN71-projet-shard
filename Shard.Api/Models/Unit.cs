038a3cb78123dba7ba0edf68e9bad3dcae5e7aa7
ZainZafar <zainzafarsial@gmail.com>
2022-11-26 20:46:38 +0100

    Added health

--- Shard.Api/Models/Unit.cs
+++ Shard.Api/Models/Unit.cs
@@ -14,23 +14,28 @@ public class Unit
     public string DestinationSystem { get; set; }
     public int Health { get; set; }
 
-    [JsonIgnore]
-    public DateTime LastUpdate { get; set; }
-    [JsonIgnore]
-    public Task MoveTask { get; set; }
-    [JsonIgnore]
-    public int ETA { get; set; }
+    [JsonIgnore] public DateTime LastUpdate { get; set; }
+    [JsonIgnore] public Task MoveTask { get; set; }
+    [JsonIgnore] public int ETA { get; set; }
 
 
     SystemSpecification SystemSpecification { get; set; }
-    
+
 
     [JsonConstructor]
     public Unit(string id, string type, string system, string planet)
     {
-        this.Id = id;
-        this.Type = type;
-        this.System = system;
-        this.Planet = planet;
+        Id = id;
+        Type = type;
+        System = system;
+        Planet = planet;
+
+        Health = type switch
+        {
+            "bomber" => 50,
+            "cruiser" => 400,
+            "fighter" => 80,
+            _ => throw new Exception("Unknown unit type")
+        };
     }
 }
